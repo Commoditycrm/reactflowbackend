bc7db8476c7ec6b7e63028ebc064eda9917f864d
irfanhathoti <irfanhathoti@gmail.com>
2025-10-28 18:53:00 +0530

    small changes

--- src/controllers/auth/login.ts
+++ src/controllers/auth/login.ts
@@ -2,22 +2,28 @@ import { Request, Response } from "express";
 import { getFirebaseAdminAuth } from "../../graphql/firebase/admin";
 import { signInWithEmailAndPassword } from "firebase/auth";
 
-
 const login = async (req: Request, res: Response) => {
-    const { email, password } = req.body
-    // if (!email || !password) {
-    //     return res.status(400).json({ message: "email and password required" })
-    // }
-    const token = await getFirebaseAdminAuth().auth().createCustomToken("rBxQi90cCeSn9pjyhWaMqaF7ecD2");
-    try {
-        // rBxQi90cCeSn9pjyhWaMqaF7ecD2
-        // const user = await signInWithEmailAndPassword(getFirebaseAdminAuth().auth(),email,password)
-        // console.log(user)
-        
-        res.status(200).json({ message: "Ok",token })
-    } catch (error) {
-        res.status(500).json(error)
-    }
-}
+  const { email, password } = req.body;
+  // if (!email || !password) {
+  //     return res.status(400).json({ message: "email and password required" })
+  // }
+  // const token = await getFirebaseAdminAuth().auth().createCustomToken("rBxQi90cCeSn9pjyhWaMqaF7ecD2");
+  try {
+    // rBxQi90cCeSn9pjyhWaMqaF7ecD2
+    // const user = await signInWithEmailAndPassword(getFirebaseAdminAuth().auth(),email,password)
+    // console.log(user)
+    const adminAuth = await getFirebaseAdminAuth().auth();
+    const user = await adminAuth.getUser('tRdnCpagQ7aImn92CG8OSZK7zhz1');
+    const currentClaims = user.customClaims || {};
+    await adminAuth.setCustomUserClaims("tRdnCpagQ7aImn92CG8OSZK7zhz1", {
+      ...currentClaims,
+      roles: ["SYSTEM_ADMIN"],
+    });
 
-export default login
+    res.status(200).json({ message: "Ok", user: currentClaims });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+export default login;
