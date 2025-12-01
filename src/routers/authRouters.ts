5a488c4a66e053cc2c225c91cb899a9e6eba1e2d
irfanhathoti <irfanhathoti@gmail.com>
2025-12-01 19:16:25 +0530

    added reset password link api

--- src/routers/authRouters.ts
+++ src/routers/authRouters.ts
@@ -2,12 +2,13 @@ import { Router } from "express";
 import login from "../controllers/auth/login";
 import createOwner from "../controllers/auth/createOrgOwner";
 import resendEmailVerifictionLink from "../controllers/auth/resendEmailVerificationLink";
+import generateResetPasswordLink from "../controllers/auth/generateResetPasswordLink";
 
 const authRouter = Router();
 
 authRouter.post("/createOwner", createOwner);
 authRouter.post("/resend_email_verification", resendEmailVerifictionLink);
-// authRouter.post("/reset_password", generateResetPassword);
+authRouter.post("/reset_password_link", generateResetPasswordLink);
 authRouter.get("/login", login);
 
 export default authRouter;
