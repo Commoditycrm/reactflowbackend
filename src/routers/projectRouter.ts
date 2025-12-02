d2dc8fd570a87eb92ade1a548770ca5cf7546d2d
irfanhathoti <irfanhathoti@gmail.com>
2025-12-02 17:31:52 +0530

    added romve user api

--- src/routers/projectRouter.ts
+++ src/routers/projectRouter.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import assignUser from "../controllers/notification/projects/assignUser";
+import removeUser from "../controllers/notification/projects/removeUser";
 
 const projectRouter = Router();
 projectRouter.post("/assign_user", assignUser);
+projectRouter.post("/remove_user", removeUser);
 
 export default projectRouter;
