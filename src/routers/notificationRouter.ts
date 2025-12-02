31d872a46247370227f6c6c0b3df82136d3d5661
irfanhathoti <irfanhathoti@gmail.com>
2025-12-02 20:42:57 +0530

    assigned task api created

--- src/routers/notificationRouter.ts
+++ src/routers/notificationRouter.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import orgRouter from "./organizationRouters";
 import projectRouter from "./projectRouter";
+import backlogItemRouter from "./backlogItemRouter";
 
 const notificationRouter = Router();
 
 notificationRouter.use("/organization", orgRouter);
 notificationRouter.use("/projects", projectRouter);
-
+notificationRouter.use("/",backlogItemRouter)
 export default notificationRouter;
