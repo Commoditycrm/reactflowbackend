473a6b65f430c30c4d8038ba8bc1d81118a41a90
irfanhathoti <irfanhathoti@gmail.com>
2026-01-06 18:39:27 +0530

    added active and deactive org email service apis

--- src/routers/organizationRouters.ts
+++ src/routers/organizationRouters.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import inviteWorkForce from "../controllers/notification/organization/invite-work-force";
 import inviteUserToOrg from "../controllers/notification/organization/invite-user";
+import orgDeactivate from "../controllers/notification/organization/deactivate";
+import activateOrg from "../controllers/notification/organization/activate";
 
 const orgRouter = Router();
 
 orgRouter.post("/invite_workforce", inviteWorkForce);
 orgRouter.post("/invite_user", inviteUserToOrg);
+orgRouter.post("/deactivate", orgDeactivate);
+orgRouter.post("/active", activateOrg);
 
 export default orgRouter;
