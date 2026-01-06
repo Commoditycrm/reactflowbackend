770ea9b7cbbf94a6b0995769819f2a7da5477574
irfanhathoti <irfanhathoti@gmail.com>
2026-01-06 17:45:30 +0530

    added event create email service ws api

--- src/routers/projectRouter.ts
+++ src/routers/projectRouter.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import assignUser from "../controllers/notification/projects/assignUser";
 import removeUser from "../controllers/notification/projects/removeUser";
+import createEventNotification from "../controllers/notification/projects/createEvent";
 
 const projectRouter = Router();
 projectRouter.post("/assign_user", assignUser);
 projectRouter.post("/remove_user", removeUser);
+projectRouter.post("/create_event", createEventNotification);
 
 export default projectRouter;
