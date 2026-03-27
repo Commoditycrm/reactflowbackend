399452bdadde0aa65e21be9df0955b8b79465949
irfanhathoti <irfanhathoti@gmail.com>
2026-03-27 13:28:04 +0530

    fixed the error

--- src/graphql/schema/schema.ts
+++ src/graphql/schema/schema.ts
@@ -6344,27 +6344,27 @@ const typeDefs = gql`
     ): Int!
       @cypher(
         statement: """
-                MATCH (org:Organization {id: $orgId})
-                CALL (org) {
-                  WITH org
-                  MATCH (org)<-[:INVITE_FOR]-(invitees:Invite)
-                  RETURN invitees AS results
-                  UNION
-                  MATCH(org)<-[:MEMBER_OF|OWNS]-(users:User)
-                  RETURN users AS results
-                }
+        MATCH (org:Organization {id: $orgId})
+        CALL (org) {
+        WITH org
+        MATCH (org)<-[:INVITE_FOR]-(invitees:Invite)
+        RETURN invitees AS results
+        UNION
+        MATCH(org)<-[:MEMBER_OF|OWNS]-(users:User)
+        RETURN users AS results
+        }
 
-                WITH DISTINCT results,
-                     trim(coalesce($emailContains, "")) AS ec,
-                     trim(coalesce($nameContains, "")) AS nc
+        WITH DISTINCT results,
+          trim(coalesce($emailContains, "")) AS ec,
+          trim(coalesce($nameContains, "")) AS nc
 
-                WHERE
-                  orgMembers.name <> "Deleted Account"
-                   AND((ec = "" AND nc = "")
-                   OR (ec <> "" AND toLower(results.email) CONTAINS toLower(ec))
-                   OR (nc <> "" AND toLower(results.name) CONTAINS toLower(nc))
-        )
-                RETURN COUNT(results) AS orgMembersCount
+          WHERE
+            results.name <> "Deleted Account"
+            AND((ec = "" AND nc = "")
+              OR (ec <> "" AND toLower(results.email) CONTAINS toLower(ec))
+              OR (nc <> "" AND toLower(results.name) CONTAINS toLower(nc))
+            )
+        RETURN COUNT(results) AS orgMembersCount
         """
         columnName: "orgMembersCount"
       )
